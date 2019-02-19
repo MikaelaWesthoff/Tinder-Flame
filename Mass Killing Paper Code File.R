e32ad5cee11bc27b680ba31b32e2304352dc912e
MikaelaWesthoff <mikaelawesthoff@gmail.com>
2019-02-19 14:11:15 -0500

    Created Script for Project

---
+++ Mass Killing Paper Code File.R
@@ -0,0 +1,4 @@
+# Code File for "Tinder and Flame: The Strategic Logic of Mass Killing" By Mikaela Westhoff
+
+setwd("~/Tinder-Flame") #Setting working directory to local version of repository
+data <- read.csv("TF-Data-2-19-19.csv")
